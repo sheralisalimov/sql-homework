129bb276e62b3c6906c5a7abf28d89f2b10fb6b3
sheralisalimov <131177307+sheralisalimov@users.noreply.github.com>
2025-05-22 05:48:20 +0500

    Create a.sql

---
+++ lesson-8/homework/a.sql
@@ -0,0 +1,28 @@
+-- Jadval yaratish:
+CREATE TABLE Products_Discounted (
+    ProductID INT PRIMARY KEY,
+    ProductName VARCHAR(100),
+    Price DECIMAL(10,2),
+    Category VARCHAR(50),
+    Stock INT
+);
+-- Ma'lumotlarni qo'shish:
+INSERT INTO Products_Discounted VALUES
+(1, 'Gaming Laptop', 950.00, 'Electronics', 25),
+(2, 'Smartphone', 750.00, 'Electronics', 45),
+(3, 'Convertible Tablet', 350.00, 'Electronics', 35),
+(4, 'Ultra-Wide Monitor', 220.00, 'Electronics', 55),
+(5, 'Mechanical Keyboard', 45.00, 'Accessories', 90),
+(6, 'Wireless Mouse', 25.00, 'Accessories', 110),
+(7, 'Chair', 130.00, 'Furniture', 75),
+(8, 'Standing Desk', 190.00, 'Furniture', 70),
+(9, 'Luxury Pen', 4.50, 'Stationery', 280),
+(10, 'Leather Notebook', 9.00, 'Stationery', 480),
+(11, 'Laser Printer', 160.00, 'Electronics', 20),
+(12, 'DSLR Camera', 480.00, 'Electronics', 35),
+(13, 'LED Flashlight', 20.00, 'Tools', 190),
+(14, 'Designer Shirt', 28.00, 'Clothing', 140),
+(15, 'Jeans', 40.00, 'Clothing', 110),
+(16, 'Winter Jacket', 70.00, 'Clothing', 60),
+(17, 'Running Shoes', 55.00, 'Clothing', 90),
+(18, 'Wool Hat', 18.00, 'Accessories', 45);
